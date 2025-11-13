412d8defa00224ce88fb92bc404aaaff170def4c
Tovchenyk <v.tovchenyk@mascode.com>
2025-11-13 17:48:37 +0800

    init commit

---
+++ src/main/java/com/deloitte/ai/DeloitteAiApplication.java
@@ -0,0 +1,13 @@
+package com.deloitte.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeloitteAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeloitteAiApplication.class, args);
+	}
+
+}
